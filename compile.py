946ceb619848a667abf19406734c4c8ff8fa899e
michaelhmorrow <michaelhmorrow98@gmail.com>
2022-08-24 13:25:29 -0400

    Simplified template

--- compile.py
+++ compile.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python3
 
-pkg_name = "M2Template" # change this to the name of your package
+pkg_name = "M2Template" # Change this to the name of your package
 
 with open("pkg_src/" + pkg_name + ".m2", "r") as file:
     lines = file.readlines()
 
-# process load commands
-reiterate = True
-while reiterate:
-    reiterate = False
+# Process load commands
+i = 0
+while True:
+    if lines[i].startswith("load "):
+        with open("pkg_src/" + lines[i].split("\"")[1]) as module_file:
+            module_lines = module_file.readlines()
+        
+        length = len(module_lines)
+        
+        # Insert lines from module
+        for j in range(length):
+            lines.insert(i + j, module_lines[j])
+        
+        i = i + length # Jump over inserted lines
+        lines[i] = "\n" # Remove load command
+    else: i = i + 1
 
-    i = 0
-    while True:
-        if lines[i].startswith("load "):
-            with open("pkg_src/" + lines[i].split("\"")[1]) as module_file:
-                module_lines = module_file.readlines()
-            
-            length = len(module_lines)
-            
-            # insert lines from module
-            for j in range(length):
-                lines.insert(i + j, module_lines[j])
-                if module_lines[j].startswith("load "): reiterate = True
-            
-            i = i + length # jump over inserted lines
-            lines[i] = "\n" # remove load command
-        else: i = i + 1
+    if i >= len(lines): break
 
-        if i >= len(lines): break
-
-# output final package
+# Output final package
 with open(pkg_name + ".m2", "w") as output:
     output.writelines(lines)
---
+++ pkg_src/ExportAndProtect.m2
@@ -0,0 +1,8 @@
+export {
+    -- Methods
+    "exampleFunction1", "exampleFunction2"
+}
+
+scan({
+    -- List symbols to be protected here
+}, protect)
